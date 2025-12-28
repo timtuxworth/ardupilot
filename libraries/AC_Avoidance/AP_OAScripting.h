c478e67e8667fb7ace6a4aa51538ab83abef9698
Tim Tuxworth <tim@objectivedata.com>
2025-12-28 13:45:23 -0700

    Plane Detect Alert Avoid

--- libraries/AC_Avoidance/AP_OAScripting.h
+++ libraries/AC_Avoidance/AP_OAScripting.h
@@ -1,8 +1,7 @@
 #pragma once
 
-
-#if AP_OAPATHPLANNER_ENABLED
-#if AP_SCRIPTING_ENABLED
+//#ifdef AP_ADSB_AVOIDANCE_ENABLED
+#ifdef AP_SCRIPTING_ENABLED
 
 #include "AC_Avoidance_config.h"
 #include "AP_OADatabase.h"
@@ -15,6 +14,8 @@
 #include "AP_OAVisGraph.h"
 #include <AP_Logger/AP_Logger_config.h>
 #include <GCS_MAVLink/GCS.h>
+
+struct OAObstacle;
 
 class AP_OAScripting {
 public:
@@ -29,7 +30,7 @@ public:
     }
 
     // enum of obstacle types for passing back to Lua
-    enum class ObstacleType : uint16_t {
+    enum class ObstacleType : uint8_t {
         GENERAL                     = 0,
         MAV_SYSID                   = 1,
         GENERAL_AVIATION            = 2,
@@ -46,49 +47,95 @@ public:
         AIS                         = 13,
     };
 
-    struct Obstacle {
-        uint32_t                    timestamp_ms;
-        uint32_t                    src_id;         // The AP_Avoid src_id
-        uint32_t                    sysid;          // The MAV_SYSID of the target -1 if n/a
-        ObstacleType                type;
-        char                        *label;
-        Vector3f                    velocity_NED_ms;
-        Location                    location;
-        Vector3f                    pos_NED_m;
-    };
+
+
+    // For efficiency don't want to loop through the obstacles multiple times. so this 
+    // returns both the closes aircraft (from ADS-B) and the closest obstacle in general which might be
+    // ADS-B Objects from AP_Avoidance
+    // Proximity objects from AP_OADatabase
+    // Fences from AC_Fence
+    bool find_threats(const Location &start_loc, const Location &end_loc, float lookahead_m,
+                                                // Return values
+                                                float       &distance_m,
+                                                OAObstacle  &any_obstacle,
+                                                OAObstacle  &aircraft_obstacle,
+                                                OAObstacle  &proximity_obstacle,
+                                                OAObstacle  &fence_obstacle
+                                                ) const;
 
     // This function is for Lua, so each Obstacle field gets returned as multiple parameters
     bool find_closest_obstacle(const Location &start_loc, const Location &end_loc, float lookahead_m,
-                                                // Return values
-                                                float    &distance_min_m, 
-                                                uint16_t &type, 
-                                                char    *&label, 
-                                                uint32_t &sys_id,
-                                                Location &location, 
-                                                Vector3f &pos_NED_m,
-                                                Vector3f &velocity_NED_ms
-                                                ) const;
+                                    // Return values
+                                    float    &distance_m, 
+                                    uint16_t &type, 
+                                    char    *&label, 
+                                    uint32_t &sys_id,
+                                    Location &location, 
+                                    Vector3f &pos_NED_m,
+                                    Vector3f &velocity_NED_ms
+                                ) const;
     
+    bool find_aircraft(const Location &vehicle_loc, const float lookahead_m,
+                                    float       &distance_m, 
+                                    OAObstacle  &aircraft_obstacle
+                            ) const;
+    // This function fines the closed crude aircraft by checking the ADS-B database
+    /*bool find_closest_aircraft(const Location &vehicle_loc, float lookahead_m,
+                                            float &distance_m, 
+                                            uint16_t &type, 
+                                            char *&label, 
+                                            uint32_t &sysid,
+                                            Location &location, 
+                                            Vector3f &pos_NED_m,
+                                            Vector3f &velocity_NED_ms
+                                            ) const;*/
+
     // This function is for Lua, so each Obstacle field gets returned as multiple parameters
     bool distance_obstacle_test(const Location &start_loc, const Location &end_loc, const float &lookahead_m, float &distance_min_m, Location &location_out) const;
     //bool distance_obstacle_test2(const float &lookahead_m, float &distance_min_m) const;
+
+    // Setup for parameters under "AVD" so "AVD_SCR"
+    static const struct AP_Param::GroupInfo var_info[];
 
 private:
 
     static AP_OAScripting *_singleton;
 
-    float _distance_to_avoidance(const Vector3f &start_NED_cm, const Vector3f &end_NED_cm, Obstacle &script_obstacle) const;
-    float _distance_to_object(const Vector3f &start_NED_m, const Vector3f end_NED_m, Obstacle &script_obstacle) const;
+    float _distance_to_avoidance(const Vector3f &start_NED_cm, const Vector3f &end_NED_cm, OAObstacle &script_obstacle, OAObstacle &aircraft_obstacle) const;
+    float _distance_to_object(const Vector3f &start_NED_m, const Vector3f end_NED_m, OAObstacle &script_obstacle) const;
+    float _distance_to_aircraft(const Vector3f &vehicle_NED_cm, const float lookahead_m, OAObstacle &script_obstacle) const;
 
-    ObstacleType _get_obstacle_type(int32_t obstacle_id) const;
-    char* _get_obstacle_label(int32_t obstacle_id) const;
+    // create a "Scripting Obstacle" to easily pass info about an obstacle to Lua
+    static void _populate_scripting_obstacle(OAObstacle &scripting_obstacle, const AP_Avoidance::Obstacle avoidance_obstacle);
+    static void _populate_fence_obstacle(OAObstacle &fence_obstacle, AP_OAScripting::ObstacleType obstacle_type);
+
+    static ObstacleType _get_obstacle_type(uint8_t emitter_type, int32_t obstacle_id);
+    static char* _get_obstacle_label(uint8_t emitter_type, int32_t obstacle_id);
 
     // Properties to work around Lua binding problem of the binding generator not being able
     // to pass in Locations and return a number of other values
     Location    _lua_start_loc;
     Location    _lua_end_loc;
 
+    // parameters
+    AP_Float _margin_aircraft;     // what is the margin around aircraft we need to avoid?
+};
+
+struct OAObstacle {
+    public:
+    uint32_t                    timestamp_ms;
+    uint32_t                    src_id;         // The AP_Avoid src_id
+    uint32_t                    icao_code;      // The ICAO code (if relevant) from ADSB
+    uint32_t                    emitter_type;   // The ADSB_EMITTER of the obstacle (if relevant)
+    bool                        is_aircraft;    // Whether or not this is an aircraft
+    bool                        is_drone;       // Whether or not this is a drone/UAV
+    uint8_t                     obstacle_type;
+    char                        *label;
+
+    Vector3f                    velocity_NED_ms;
+    Location                    location;
+    Vector3f                    position_NED_m;
 };
 
 #endif
-#endif
+//#endif
